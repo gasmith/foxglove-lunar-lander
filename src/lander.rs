6466fdfb86e51509782b6edad2a88ac72163ea7f
Greg Smith <gasmith@foxglove.dev>
2025-03-26 09:33:12 -0700

    rework landing report

--- src/lander.rs
+++ src/lander.rs
@@ -1,8 +1,10 @@
 use foxglove::schemas::{
-    FrameTransform, ModelPrimitive, Pose, Quaternion, SceneEntity, SceneUpdate, Vector3,
+    Color, FrameTransform, ModelPrimitive, Pose, Quaternion, SceneEntity, SceneEntityDeletion,
+    SceneUpdate, TextPrimitive, Vector3,
 };
 use foxglove::static_typed_channel;
 use glam::{EulerRot, Quat, Vec3};
+use serde::Serialize;
 
 use crate::controls::Controls;
 use crate::convert::IntoFg;
@@ -14,6 +16,9 @@ static_typed_channel!(LANDER_ANGULAR_VELOCITY, "/lander_angular_velocity", Vecto
 static_typed_channel!(LANDER_VELOCITY, "/lander_velocity", Vector3);
 static_typed_channel!(LANDER_ORIENTATION, "/lander_orientation", Quaternion);
 static_typed_channel!(LANDER_FUEL_MASS, "/lander_fuel_mass", Metric);
+static_typed_channel!(LANDING_REPORT, "/landing_report", LandingReport);
+static_typed_channel!(BANNER, "/banner", SceneUpdate);
+static_typed_channel!(BANNER_FT, "/banner_ft", FrameTransform);
 
 /// Base mass for the apollo lander.
 const APOLLO_LANDER_DRY_MASS_KG: f32 = 2_150.0;
@@ -50,14 +55,51 @@ const MOON_GRAVITY: Vec3 = Vec3 {
     z: -1.62,
 };
 
-#[derive(Debug, Clone, Copy)]
-pub enum LanderStatus {
-    Aloft,
+#[derive(Debug, Clone, Copy, Serialize, schemars::JsonSchema)]
+#[serde(rename = "snake_case")]
+pub enum LandingStatus {
     Landed,
-    TooFast,
-    NotLevel,
-    Spinning,
     Missed,
+    Crashed,
+}
+
+#[derive(Debug, Clone, Copy, Serialize, schemars::JsonSchema)]
+#[serde(rename = "snake_case")]
+pub enum LandingCriterionType {
+    VerticalSpeed,
+    HorizontalSpeed,
+    Tilt,
+    AngularSpeed,
+    DistanceFromLandingZone,
+}
+
+#[derive(Debug, Clone, Copy, Serialize, schemars::JsonSchema)]
+pub struct LandingCriterion {
+    r#type: LandingCriterionType,
+    max: f32,
+    actual: f32,
+}
+impl LandingCriterion {
+    fn ok(&self) -> bool {
+        self.actual <= self.max
+    }
+
+    fn score(&self) -> f32 {
+        (self.max - self.actual).max(0.0) / self.max
+    }
+}
+
+#[derive(Debug, Default, Clone, Serialize, schemars::JsonSchema)]
+pub struct LandingReport {
+    report: Option<LandingReportInner>,
+}
+
+#[derive(Debug, Clone, Serialize, schemars::JsonSchema)]
+pub struct LandingReportInner {
+    status: LandingStatus,
+    summary: String,
+    score: f32,
+    criteria: Vec<LandingCriterion>,
 }
 
 pub struct Lander {
@@ -128,27 +170,85 @@ impl Lander {
         );
     }
 
-    pub fn status(&self) -> LanderStatus {
-        if self.position.z > 0.0 {
-            LanderStatus::Aloft
-        } else if self.vertical_velocity_mag() > 1.0 || self.horizontal_velocity_mag() > 1.0 {
-            LanderStatus::TooFast
-        } else if self.tilt_radians() > 0.25 {
-            LanderStatus::NotLevel
-        } else if self.angular_velocity.length() > 0.5 {
-            LanderStatus::Spinning
-        } else if self.distance_from_center() > self.landing_zone_radius as f32 {
-            LanderStatus::Missed
-        } else {
-            LanderStatus::Landed
-        }
+    pub fn has_landed(&self) -> bool {
+        self.position.z <= 0.0
     }
 
-    fn vertical_velocity_mag(&self) -> f32 {
+    fn landing_report(&self) -> Option<LandingReportInner> {
+        if !self.has_landed() {
+            return None;
+        }
+
+        let criteria = self.landing_criteria();
+        let mut score = 0.0;
+        let mut first_problem = None;
+        for crit in &criteria {
+            if !crit.ok() && first_problem.is_none() {
+                first_problem = Some(crit.r#type);
+            }
+            score += crit.score() * 2.0;
+        }
+        let status = match first_problem {
+            None => LandingStatus::Landed,
+            Some(LandingCriterionType::DistanceFromLandingZone) => LandingStatus::Missed,
+            Some(_) => LandingStatus::Crashed,
+        };
+        let summary = match first_problem {
+            Some(LandingCriterionType::VerticalSpeed) => {
+                "The lander redefined the term 'lunar impactor'. NASA's department of craters thanks you."
+            }
+            Some(LandingCriterionType::HorizontalSpeed) => {
+                "You landed... sideways. The ground wasn't ready for that level of enthusiasm."
+            }
+            Some(LandingCriterionType::Tilt) => {
+                "You came in like a majestic leaning tower of 'nope'."
+            }
+            Some(LandingCriterionType::AngularSpeed) => {
+                "Still spinning on landing; were you trying for a celebratory twirl?"
+            }
+            Some(LandingCriterionType::DistanceFromLandingZone) => {
+                "You stuck the landing - on the wrong part of the moon."
+            }
+            None => "The eagle has landed.",
+        };
+        Some(LandingReportInner {
+            status,
+            summary: summary.to_string(),
+            score,
+            criteria,
+        })
+    }
+
+    fn landing_criteria(&self) -> Vec<LandingCriterion> {
+        macro_rules! criterion {
+            ($variant:tt, $max:expr, $eval:expr) => {
+                LandingCriterion {
+                    r#type: LandingCriterionType::$variant,
+                    max: $max,
+                    actual: $eval,
+                }
+            };
+        }
+        vec![
+            criterion!(VerticalSpeed, 1.0, self.vertical_speed()),
+            criterion!(HorizontalSpeed, 1.0, self.horizontal_speed()),
+            criterion!(Tilt, 0.25, self.tilt()),
+            criterion!(AngularSpeed, 0.25, self.angular_speed()),
+            criterion!(
+                DistanceFromLandingZone,
+                self.landing_zone_radius as f32,
+                self.distance_from_landing_zone()
+            ),
+        ]
+    }
+
+    /// Vertical speed in meters/s.
+    fn vertical_speed(&self) -> f32 {
         self.velocity.z.abs()
     }
 
-    fn horizontal_velocity_mag(&self) -> f32 {
+    /// Horizontal speed in meters/s.
+    fn horizontal_speed(&self) -> f32 {
         Vec3 {
             x: self.velocity.x,
             y: self.velocity.y,
@@ -157,18 +257,20 @@ impl Lander {
         .length()
     }
 
-    fn distance_from_center(&self) -> f32 {
-        Vec3 {
-            x: self.position.x,
-            y: self.position.y,
-            z: 0.0,
-        }
-        .length()
-    }
-
-    fn tilt_radians(&self) -> f32 {
+    /// Tilt from upright in radians.
+    fn tilt(&self) -> f32 {
         let up = self.rotation * Vec3::Z;
         up.angle_between(Vec3::Z)
+    }
+
+    /// Angular speed in radians/s.
+    fn angular_speed(&self) -> f32 {
+        self.angular_velocity.length()
+    }
+
+    /// Distance from landing zone in meters.
+    fn distance_from_landing_zone(&self) -> f32 {
+        self.position.length()
     }
 
     fn frame_transform(&self) -> FrameTransform {
@@ -207,6 +309,61 @@ impl Lander {
         LANDER.log(&SceneUpdate {
             entities: vec![self.scene_entity()],
             ..Default::default()
+        });
+    }
+
+    pub fn clear_landing_report(&self) {
+        BANNER_FT.log_static(&FrameTransform {
+            parent_frame_id: "lander".into(),
+            child_frame_id: "banner".into(),
+            ..Default::default()
+        });
+        BANNER.log_static(&SceneUpdate {
+            deletions: vec![SceneEntityDeletion {
+                id: "banner".into(),
+                ..Default::default()
+            }],
+            ..Default::default()
+        });
+        LANDING_REPORT.log_static(&LandingReport::default());
+    }
+
+    pub fn log_landing_report(&self) {
+        let Some(report) = self.landing_report() else {
+            return;
+        };
+        let ((r, g, b), text) = match report.status {
+            LandingStatus::Landed => ((0.0, 1.0, 0.0), "LANDED"),
+            LandingStatus::Missed => ((1.0, 1.0, 0.0), "MISSED"),
+            LandingStatus::Crashed => ((1.0, 0.0, 0.0), "YOU DIED"),
+        };
+        BANNER_FT.log_static(&FrameTransform {
+            parent_frame_id: "lander".into(),
+            child_frame_id: "banner".into(),
+            translation: Some(Vector3 {
+                z: 5.0,
+                ..Default::default()
+            }),
+            ..Default::default()
+        });
+        BANNER.log_static(&SceneUpdate {
+            entities: vec![SceneEntity {
+                frame_id: "banner".into(),
+                id: "banner".into(),
+                texts: vec![TextPrimitive {
+                    pose: None,
+                    billboard: true,
+                    font_size: 48.0,
+                    scale_invariant: true,
+                    color: Some(Color { r, g, b, a: 0.75 }),
+                    text: text.to_string(),
+                }],
+                ..Default::default()
+            }],
+            ..Default::default()
+        });
+        LANDING_REPORT.log_static(&LandingReport {
+            report: Some(report),
         });
     }
 }
--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,7 @@
 use std::time::Duration;
 
-use foxglove::schemas::{
-    Color, FrameTransform, SceneEntity, SceneEntityDeletion, SceneUpdate, TextPrimitive, Vector3,
-};
+use foxglove::WebSocketServer;
 use foxglove::websocket::Capability;
-use foxglove::{WebSocketServer, static_typed_channel};
 use rand::prelude::*;
 use rand_chacha::ChaCha8Rng;
 
@@ -18,13 +15,10 @@ mod message;
 mod parameters;
 
 use controls::Controls;
-use lander::{Lander, LanderStatus};
+use lander::Lander;
 use landscape::Landscape;
 use listener::Listener;
 use parameters::Parameters;
-
-static_typed_channel!(BANNER, "/banner", SceneUpdate);
-static_typed_channel!(BANNER_FT, "/banner_ft", FrameTransform);
 
 const GAME_STEP_DURATION: Duration = Duration::from_millis(33);
 
@@ -61,116 +55,25 @@ async fn game_loop(params: Parameters, controls: Controls) {
             landscape.lander_start_position(),
             params.landing_zone_radius(),
         );
+        lander.clear_landing_report();
         landscape.log_static();
         controls.do_reset();
-        clear_banner();
         game_round(lander, &controls).await;
     }
 }
 
 async fn game_round(mut lander: Lander, controls: &Controls) {
-    let mut status = LanderStatus::Aloft;
+    let mut landed = false;
     while !controls.is_reset_pending() {
-        if matches!(status, LanderStatus::Aloft) {
+        if !landed {
             lander.step(GAME_STEP_DURATION.as_secs_f32(), controls);
-            status = lander.status();
-            if !matches!(status, LanderStatus::Aloft) {
-                display_banner(status);
+            if lander.has_landed() {
+                landed = true;
+                lander.log_landing_report();
                 lander.stop();
             }
         }
         lander.log();
         tokio::time::sleep(GAME_STEP_DURATION).await;
     }
-}
-
-fn clear_banner() {
-    BANNER_FT.log_static(&FrameTransform {
-        parent_frame_id: "lander".into(),
-        child_frame_id: "banner".into(),
-        ..Default::default()
-    });
-    BANNER.log_static(&SceneUpdate {
-        deletions: vec![SceneEntityDeletion {
-            id: "banner".into(),
-            ..Default::default()
-        }],
-        ..Default::default()
-    });
-}
-
-fn display_banner(status: LanderStatus) {
-    let (color, text) = match status {
-        LanderStatus::Aloft => return,
-        LanderStatus::Landed => (
-            Color {
-                r: 0.0,
-                g: 1.0,
-                b: 0.0,
-                a: 0.75,
-            },
-            "LANDED".to_string(),
-        ),
-        LanderStatus::TooFast => (
-            Color {
-                r: 1.0,
-                g: 0.0,
-                b: 0.0,
-                a: 0.75,
-            },
-            "TOO FAST".to_string(),
-        ),
-        LanderStatus::NotLevel => (
-            Color {
-                r: 1.0,
-                g: 0.0,
-                b: 0.0,
-                a: 0.75,
-            },
-            "NOT LEVEL".to_string(),
-        ),
-        LanderStatus::Spinning => (
-            Color {
-                r: 1.0,
-                g: 0.0,
-                b: 0.0,
-                a: 0.75,
-            },
-            "SPINNING".to_string(),
-        ),
-        LanderStatus::Missed => (
-            Color {
-                r: 1.0,
-                g: 1.0,
-                b: 0.0,
-                a: 0.75,
-            },
-            "MISSED".to_string(),
-        ),
-    };
-    BANNER_FT.log_static(&FrameTransform {
-        parent_frame_id: "lander".into(),
-        child_frame_id: "banner".into(),
-        translation: Some(Vector3 {
-            z: 5.0,
-            ..Default::default()
-        }),
-        ..Default::default()
-    });
-    BANNER.log_static(&SceneUpdate {
-        entities: vec![SceneEntity {
-            frame_id: "banner".into(),
-            id: "banner".into(),
-            texts: vec![TextPrimitive {
-                pose: None,
-                billboard: true,
-                font_size: 48.0,
-                scale_invariant: true,
-                color: Some(color),
-                text,
-            }],
-            ..Default::default()
-        }],
-        ..Default::default()
-    });
 }
