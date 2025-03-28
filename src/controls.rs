3b97627837b98cdca28a6e705759d2df5b90fbf5
Greg Smith <gasmith@foxglove.dev>
2025-03-28 09:06:34 -0700

    update layouts, put repeater on vv buttons

--- src/controls.rs
+++ src/controls.rs
@@ -1,4 +1,7 @@
-use std::sync::Arc;
+use std::{
+    sync::Arc,
+    time::{Duration, Instant},
+};
 
 use glam::{Vec2, Vec3};
 use parking_lot::RwLock;
@@ -16,7 +19,7 @@ impl Controls {
 
     pub fn get_and_reset_vertical_velocity_delta(&self) -> f32 {
         let mut inner = self.0.write();
-        0.1 * (inner.inc_vertical_velocity.get_and_reset() as f32
+        0.2 * (inner.inc_vertical_velocity.get_and_reset() as f32
             - inner.dec_vertical_velocity.get_and_reset() as f32)
     }
 
@@ -60,7 +63,6 @@ impl Controls {
     }
 }
 
-#[derive(Default)]
 struct Inner {
     reset: Button,
     strafe: Vec2,
@@ -68,19 +70,47 @@ struct Inner {
     inc_vertical_velocity: Button,
     dec_vertical_velocity: Button,
 }
+impl Default for Inner {
+    fn default() -> Self {
+        let repeat = Duration::from_millis(100);
+        Self {
+            reset: Button::default(),
+            strafe: Vec2::default(),
+            rotate: Vec3::default(),
+            inc_vertical_velocity: Button::with_repeater(repeat),
+            dec_vertical_velocity: Button::with_repeater(repeat),
+        }
+    }
+}
 
 #[derive(Default)]
 struct Button {
     pressed: bool,
     count: u32,
+    timeout: Option<Duration>,
+    deadline: Option<Instant>,
 }
 
 impl Button {
+    fn with_repeater(timeout: Duration) -> Self {
+        Self {
+            timeout: Some(timeout),
+            ..Default::default()
+        }
+    }
+
     fn update(&mut self, pressed: bool) {
+        if self.deadline.is_some_and(|t| t > Instant::now()) {
+            self.pressed = false;
+            self.deadline = None;
+        }
         match (pressed, self.pressed) {
             (true, false) => {
                 self.pressed = true;
                 self.count += 1;
+                if let Some(timeout) = self.timeout {
+                    self.deadline = Some(Instant::now() + timeout);
+                }
             }
             (false, true) => {
                 self.pressed = false;
@@ -103,6 +133,7 @@ impl Button {
         self.count = 0;
         if hard {
             self.pressed = false;
+            self.deadline = None;
         }
     }
 }
